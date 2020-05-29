23591fadaccc052652bf3a67c54554b545d47cfe
k0o <sb.knot@gmail.com>
2020-05-29 11:44:00 -0400

    basics

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 const app = express();
 
+// Settings
+app.set('view engine', ejs);
+app.set('views', path.join(__dirname, 'views'));
+
+// Middlewares
+app.use(express.static(path.join(__dirname, 'assets')));
+
 app.get('/', (req, res) => {
-    res.send('root');
-})
+    res.render('index.ejs');
+});
 
 app.listen(3000, () => console.log('Servidor en puerto 3000'));
