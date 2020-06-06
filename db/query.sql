aff0a91349404ccbfd5b4b1ed43bab1899bafeaa
Johan809 <20186764@itla.edu.do>
2020-06-06 10:25:36 -0400

    Query CasoF3

--- db/query.sql
+++ db/query.sql
@@ -21,8 +21,8 @@ CREATE TABLE trabajo
     ubicacion VARCHAR(50) NOT NULL,
     posicion VARCHAR(50) NOT NULL,
     compañia VARCHAR(50) NOT NULL,
-    fecha date NOT NULL
-	
+    fecha date NOT NULL,
+	categoria VARCHAR(50) NOT NULL
 );
 
 ALTER TABLE trabajo
@@ -47,6 +47,53 @@ CREATE TABLE TIPO
 	
 );
 
-ALTER TABLE enviarPuestoJob ADD FOREIGN KEY (IDJOB) REFERENCES TRABAJO(ID)
+ALTER TABLE enviarPuestoJob ADD FOREIGN KEY (IDJOB) REFERENCES TRABAJO(ID);
+GO
 
+--Query para el caso F3
+--drop proc CASOF3
+delimiter//
+Create Proc CASOF3(@busqueda varchar(255))
+AS
+BEGIN
+	Create Table #tmpC3
+	(
+		ID int(11),
+		ubicacion VARCHAR(50),
+		posicion VARCHAR(50),
+		compañia VARCHAR(50),
+		fecha date,
+		categoria VARCHAR(50)
+	)
 
+	Declare @ID int(11);
+	Declare	@ubicacion VARCHAR(50);
+	Declare	@posicion VARCHAR(50);
+	Declare	@compañia VARCHAR(50);
+	Declare	@fecha date;
+	Declare	@categoria VARCHAR(50);
+
+	Declare C_Datos Cursor for
+	--Aqui va le query cabrona
+	Select * from trabajo where categoria like @busqueda or ubicacion like @busqueda 
+	or posicion like @busqueda or compañia like @busqueda or fecha like @busqueda;
+
+	Open C_Datos
+	while 1=1
+	begin
+		fetch next from C_Datos into @ID, @ubicacion, @posicion, @compañia, @fecha, @categoria
+		if @@FETCH_STATUS != 0
+		begin
+			break
+		end
+
+		Insert Into #tmpC3 values(@ID, @ubicacion, @posicion, @compañia, @fecha, @categoria)
+	end
+	Close C_Datos
+	Deallocate C_Datos
+
+	Select * from #tmpC3
+END
+//
+delimiter;
+Go
