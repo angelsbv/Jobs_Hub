953d901ee44908ab84aeca1bedcec20887936213
k0o <sb.knot@gmail.com>
2020-06-06 00:01:08 -0400

    patch: conflicto de Error 404 con rutas /job

--- src/routes/jobs.routes.js
+++ src/routes/jobs.routes.js
@@ -12,19 +12,19 @@ const router = require('express').Router();
 const bLayout = 'layouts/B';
 const mLayout = 'layouts/M';
 
-router.get('/postjob', (req, res) => {
+router.get('/post', (req, res) => {
     res.render('post-job', {
         layout: bLayout
     });
 });
 
-router.get('/job-details', (req, res) => {
+router.get('/details', (req, res) => {
     res.render('job-details', {
         layout: bLayout
     });
 });
 
-router.get('/job-category', (req, res) => {
+router.get('/category', (req, res) => {
     res.render('job-category', {
         layout: bLayout
     })
