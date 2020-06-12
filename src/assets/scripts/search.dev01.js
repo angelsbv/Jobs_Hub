cf1a433e7456a3c291f5c608df732395788263fe
k0o <sb.knot@gmail.com>
2020-06-11 21:58:41 -0400

    página principal: search

---
+++ src/assets/scripts/search.dev01.js
@@ -0,0 +1,68 @@
+const resultsTemplate = `
+<div class="category">
+    <h1>Resultados Coincidentes</h1>
+    <table class="table tabla-design table-hover">
+        <thead class="thead-light">
+            <tr>
+                <th>Location</th>
+                <th>Position</th>
+                <th>Company</th>
+                <th>Category</th>
+            </tr>
+        </thead>
+        <tbody class="results">
+        </tbody>
+    </table>
+</div>
+`
+const searchForm = document.querySelector('#search-form');
+const searchField = document.querySelector('#search-field');
+
+const searchFormSubmitHandler = (e) => {
+    e.preventDefault();
+    if(searchField.value.trim().length > 0){
+        getResults()
+        .then(({ results }) => {
+            if(results.length > 0){
+                container.innerHTML = resultsTemplate;
+                const resultsTbody = document.querySelector('.results');
+                for(let i in results){
+                    let job = results[i];
+                    let tr = document.createElement("tr");
+                    tr.id = job.ID;
+                    tr.style.cursor = "pointer";
+                    for(let t in job)
+                    {       
+                        if(t === 'ubicacion' 
+                        || t === 'posicion' 
+                        || t === 'compañia'
+                        || t === 'categoria'
+                        ){
+                            let td = document.createElement('td');
+                            td.innerHTML = job[t]; 
+                            tr.appendChild(td);
+                        }
+                    }
+                    resultsTbody.appendChild(tr);
+                }
+            }else{
+                container.innerHTML = 'No pudimos encontrar resultados, intente con palabras distintas.';
+            }
+        })
+        .catch((err) => {
+            throw err;
+        });
+    }
+}
+
+const getResults = async () => {
+    try {
+        const resp = await fetch (`/job/search/${encodeURIComponent(searchField.value)}`)
+        const data = await resp.json();
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+searchForm.addEventListener('submit', searchFormSubmitHandler);
