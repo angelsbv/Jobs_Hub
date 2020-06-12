eb8086b0a8abb8da2308dc28bcf7a6e6af5d3493
k0o <sb.knot@gmail.com>
2020-06-12 18:01:20 -0400

    adm: /set-site-config

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,6 +3,9 @@ const path = require('path');
 const { promisify } = require('util');
 const fs = require('fs');
 const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+const configPath = path.join(__dirname, '../site.config.json');
 
 const forAdmins = (req, res, next) => {
     const { userRol } = req.user
@@ -14,7 +17,7 @@ const forAdmins = (req, res, next) => {
 
 router.get('/get-site-config', forAdmins, async (req, res) => {
     try {
-        let data = await (await readFile(path.join(__dirname, '../site.config.json')));
+        const data = await readFile(configPath);
         res.json({
             ok: true, 
             config: JSON.parse(data.toString())
@@ -26,6 +29,36 @@ router.get('/get-site-config', forAdmins, async (req, res) => {
         });
         throw error;
     }
-})
+});
+
+router.post('/set-site-config', forAdmins, async (req, res) => {
+    try {
+        const { body } = req;
+        await writeFile(configPath, JSON.stringify(body, null, 2), 'utf8');
+        const data = await readFile(configPath);
+        res.json({
+            ok: true, 
+            config: JSON.parse(data.toString())
+        });
+    } catch (error) {
+        res.json({
+            ok: false,
+            error: 'Hubo un error con su solicitud' 
+        });
+        throw error;
+    }
+});
+
+// let resp = await fetch('/adm/get-site-config');
+// let data = await resp.json();
+// console.log(data);
+
+// resp = await fetch('/adm/set-site-config', {
+//     method: 'POST',
+//     headers:{ "Content-Type": "application/json" },
+//     body: JSON.stringify(data.config)
+// });
+// data = await resp.json();
+// console.log(data);
 
 module.exports = router;
